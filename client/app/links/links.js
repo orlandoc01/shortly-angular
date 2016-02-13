211ab31f80d10171e2e751a533a6a696d23dbe79
orlandoc01 <orlandoc@sas.upenn.edu>
2016-02-12 17:11:03 -0800

    Add signout button and signout method to links and shorten js/html files

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -1,9 +1,11 @@
 angular.module('shortly.links', [])
 
-.controller('LinksController', function ($scope, Links) {
+.controller('LinksController', function ($scope, Links, Auth) {
   // Your code here
   $scope.data = {};
   Links.getAll().then(function (links) {
     $scope.data.links = links;
   });
+
+  $scope.signout = Auth.signout;
 });
--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -1,9 +1,12 @@
 angular.module('shortly.shorten', [])
 
-.controller('ShortenController', function ($scope, $location, Links) {
+.controller('ShortenController', function ($scope, $location, Links, Auth) {
   // Your code here
   $scope.link = {};
   $scope.addLink = function () {
     Links.addOne($scope.link);
   };
+
+  $scope.signout = Auth.signout;
+
 });
