4911d604d15b04a569b0aebaa18c781588034138
orlandoc01 <orlandoc@sas.upenn.edu>
2016-02-12 19:58:31 -0800

    Add styling to links that are displayed

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -4,12 +4,10 @@ angular.module('shortly.links', [])
 
   $scope.data = {};
 
-
   Links.getAll().then(function (links) {
     links.sort(function (link1, link2) {
       return link2.visits - link1.visits;
     });
-    window.links = links;
     $scope.data.links = links;
   });
 
