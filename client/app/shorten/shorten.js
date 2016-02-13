fe9d3af451011d1bd9ea9bb8e7e9774dab268f27
orlandoc01 <orlandoc@sas.upenn.edu>
2016-02-12 17:24:47 -0800

    Add input for shorten page, add check for valid url in shortenController

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -2,9 +2,22 @@ angular.module('shortly.shorten', [])
 
 .controller('ShortenController', function ($scope, $location, Links, Auth) {
   // Your code here
-  $scope.link = {};
+  var rValidUrl = /^(?!mailto:)(?:(?:https?|ftp):\/\/)?(?:\S+(?::\S*)?@)?(?:(?:(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[0-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))|localhost)(?::\d{2,5})?(?:\/[^\s]*)?$/i;
+
+  var isValidUrl = function (url) {
+    return url.match(rValidUrl);
+  };
+
+  $scope.link = {
+    url: ""
+  };
+
   $scope.addLink = function () {
-    Links.addOne($scope.link);
+    console.log('$scope.link', $scope.link);
+    var check = isValidUrl($scope.link.url);
+    if (check) {
+      Links.addOne($scope.link);
+    }
   };
 
   $scope.signout = Auth.signout;
