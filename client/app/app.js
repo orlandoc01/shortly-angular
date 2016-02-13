77956fd4e09e03f46f1e3cfc8982caf37cab374a
orlandoc01 <orlandoc@sas.upenn.edu>
2016-02-12 16:22:01 -0800

    Implement default redirect to signin page

--- client/app/app.js
+++ client/app/app.js
@@ -23,6 +23,9 @@ angular.module('shortly', [
     .when('/shorten', {
       templateUrl: 'app/shorten/shorten.html',
       controller: 'ShortenController'
+    })
+    .otherwise({
+      redirectTo: '/signin'
     });
     // We add our $httpInterceptor into the array
     // of interceptors. Think of it like middleware for your ajax calls
