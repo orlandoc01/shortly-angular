b4d246b78365680eeae05dd4d25ab97ed66aa18f
orlandoc01 <orlandoc@sas.upenn.edu>
2016-02-12 19:26:33 -0800

    Implement live search option in links tab

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -1,16 +1,18 @@
 angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links, Auth) {
-  // Your code here
+
   $scope.data = {};
+
+
   Links.getAll().then(function (links) {
     links.sort(function (link1, link2) {
       return link2.visits - link1.visits;
     });
-    $scope.data.links = links;
     window.links = links;
-    console.log($scope.data);
+    $scope.data.links = links;
   });
 
   $scope.signout = Auth.signout;
+
 });
